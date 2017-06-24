caaabbaefd9cc954637d3f70b805e44a7fef212d
msverybrightside <msverybrightside@gmail.com>
2017-06-24 16:59:27 +0200

    settings allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nnx02vq#zg22x*r6jpb)ok7sf@x&)8qx+*5bllkl4^)umsn(m+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['msverybrightside.pythonanywhere.com',]
 
 
 # Application definition
