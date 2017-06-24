4cc76fa0225bd64d7efe9d549fd2d5301418a12c
msverybrightside <msverybrightside@gmail.com>
2017-06-24 16:30:01 +0200

    fixing static url

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,4 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATiC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
